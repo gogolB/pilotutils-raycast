cab51849086324fa8eb7ed9b8958615bc4ef73a9
Gogol Bhattacharya <acx234@gmail.com>
2024-02-03 19:27:26 -0600

    Finished up metar gathering

--- src/metar.tsx
+++ src/metar.tsx
@@ -2,6 +2,7 @@ import { Form, LaunchProps } from "@raycast/api";
 import { List } from "@raycast/api";
 import { useEffect, useState } from "react";
 import fetch from "node-fetch"
+import { v4 as uuidv4 } from 'uuid';
 
 interface State {
     icao?: string;
@@ -16,6 +17,7 @@ type METAR_DATA = {
     receiptTime: string,
     obsTime: number,
     reportTime: string,
+    reportTime_local: string,
     temp: number,
     dewp: number,
     wdir: number,
@@ -45,22 +47,59 @@ type METAR_DATA = {
     elev: number,
     prior: number,
     name: string,
-    clouds: [ [any] ]
-  }
+    clouds: Array<CLOUD>,
+    pressure_alt: number,
+    density_alt: number,
+}
+
+type CLOUD = {
+    cover: string,
+    base: number,
+}
 
 
 async function fetchMetar(icao: string): Promise<Array<METAR_DATA>>{
     const response = await fetch(`https://aviationweather.gov/api/data/metar?ids=${icao}&format=json`);
     const data = await response.json() as Array<METAR_DATA>;
+    data.forEach((metar) => {
+        const rt = new Date(metar.reportTime + "Z");
+        metar.reportTime_local = rt.toLocaleString();
+        metar.altim = convertMbToInHg(metar.altim);
+        metar.pressure_alt = Math.round((29.92 - metar.altim) * 1000) + metar.elev;
+        metar.density_alt = calculateDensityAltitude(metar);
+    });
     return data;
 }
+
+function convertMbToInHg(mb: number): number {
+    const inHg = mb * 0.02953;
+    return inHg;
+}
+
+
+function calculateDensityAltitude(metar: METAR_DATA): number {
+    const temperature = metar.temp;
+    const pressureAltitude = metar.pressure_alt;
+
+    const temperatureInCelsius = (temperature - 32) * (5 / 9);
+    const temperatureInKelvin = temperatureInCelsius + 273.15;
+
+    const pressureInHg = metar.altim;
+    const pressureInHpa = pressureInHg * 33.8639;
+
+    const pressureAtSeaLevelInHpa = pressureInHpa / Math.pow(1 - (0.0065 * pressureAltitude) / temperatureInKelvin, 5.2561);
+
+    const densityAltitude = (temperatureInKelvin / 0.0065) * (1 - Math.pow(pressureAtSeaLevelInHpa / pressureInHpa, 0.190284)) - 273.15;
+
+    return Math.round(densityAltitude);
+}
+
 
 
 export default function METAR(props: LaunchProps<{ arguments: Arguments.Metar }>) {
     const [state, setState] = useState<State>({});
 
     const icao = props.arguments.icao;
-    console.log(`icao: ${icao}`)
 
     const req = `https://aviationweather.gov/api/data/metar?ids=${icao}&format=json`
 
@@ -80,38 +119,47 @@ export default function METAR(props: LaunchProps<{ arguments: Arguments.Metar }>
         getMetar();
     }, []);
 
-    console.log(state.response?.at(0)?.rawOb);
-    
-
     return (
         <List isLoading={!state.response && !state.error} isShowingDetail> 
             {state.response?.map((metar) => (
                 <List.Item
                     key={metar.metar_id}
-                    title={metar.name}
-                    subtitle={metar.rawOb}
+                    title={metar.icaoId}
+                    subtitle={metar.name}
                     detail={
                         <List.Item.Detail
+                            markdown={` \# ${metar.icaoId}\n\#\# ${metar.name}\n\`\`\`${metar.rawOb}\`\`\``}
                             metadata={
                                 <List.Item.Detail.Metadata>
                                     <List.Item.Detail.Metadata.Label title="ICAO" text={metar.icaoId}/>
                                     <List.Item.Detail.Metadata.Label title="Name" text={metar.name}/>
                                     <List.Item.Detail.Metadata.Label title="Latitude" text={metar.lat.toString()}/>
                                     <List.Item.Detail.Metadata.Label title="Longitude" text={metar.lon.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Elevation" text={metar.elev.toString()}/>
-                                    <List.Item.Detail.Metadata.Separator />
-                                    <List.Item.Detail.Metadata.Label title="Raw Observation" />
-                                    <List.Item.Detail.Metadata.Label title={metar.rawOb.toString()} />
+                                    <List.Item.Detail.Metadata.Label title="Field Elevation" text={`${metar.elev.toString()}ft`}/>
+                                    <List.Item.Detail.Metadata.Label title="Pressure Altitude" text={`${metar.pressure_alt.toString()}ft`}/>
+                                    <List.Item.Detail.Metadata.Label title="Density Altitude" text={`${metar.density_alt.toString()}ft`}/>
                                     <List.Item.Detail.Metadata.Separator />
                                     <List.Item.Detail.Metadata.Label title="Observation Time" text={metar.obsTime.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Report Time" text={metar.reportTime.toString()}/>
+                                    <List.Item.Detail.Metadata.Label title="Report Time (Zulu)" text={metar.reportTime.toString()}/>
+                                    <List.Item.Detail.Metadata.Label title="Report Time (Local)" text={metar.reportTime_local.toString()}/>
                                     <List.Item.Detail.Metadata.Separator />
-                                    <List.Item.Detail.Metadata.Label title="Temperature" text={metar.temp.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Dewpoint" text={metar.dewp.toString()}/>
+                                    <List.Item.Detail.Metadata.Label title="Temperature" text={`${metar.temp.toString()} °C`}/>
+                                    <List.Item.Detail.Metadata.Label title="Dewpoint" text={`${metar.dewp.toString()} °C`}/>
+                                    <List.Item.Detail.Metadata.Label title="Altimeter" text={`${metar.altim.toString()} inHg`}/>
                                     <List.Item.Detail.Metadata.Label title="Wind Direction" text={metar.wdir.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Wind Speed" text={metar.wspd.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Visibility" text={metar.visib.toString()}/>
-                                    <List.Item.Detail.Metadata.Label title="Altimeter" text={metar.altim.toString()}/>
+                                    <List.Item.Detail.Metadata.Label title="Wind Speed" text={`${metar.wspd.toString()} Knots`}/>
+                                    {metar.wgst && <List.Item.Detail.Metadata.Label title="Wind Gust" text={`${metar.wgst.toString()} Knots`}/>}
+                                    <List.Item.Detail.Metadata.Separator />
+                                    <List.Item.Detail.Metadata.Label title="Visibility" text={`${metar.visib.toString()} SM`}/>
+                                    {metar.wxString && <List.Item.Detail.Metadata.Label title="Weather" text={metar.wxString}/>}
+                                    {metar.pcp3hr && <List.Item.Detail.Metadata.Label title="3 Hour Precipitation" text={metar.pcp3hr.toString()}/>}
+                                    {metar.pcp6hr && <List.Item.Detail.Metadata.Label title="6 Hour Precipitation" text={metar.pcp6hr.toString()}/>}
+                                    {metar.pcp24hr && <List.Item.Detail.Metadata.Label title="24 Hour Precipitation" text={metar.pcp24hr.toString()}/>}
+                                    {metar.snow && <List.Item.Detail.Metadata.Label title="Snow" text={metar.snow.toString()}/>}
+                                    {metar.clouds && <List.Item.Detail.Metadata.Separator />}
+                                    {metar.clouds && metar.clouds.map((cloud) => (
+                                        <List.Item.Detail.Metadata.Label key={uuidv4()} title={`${cloud.cover}`} text={`${cloud.base}ft`}/>
+                                    ))}
                                 </List.Item.Detail.Metadata>
                             }
                         />
